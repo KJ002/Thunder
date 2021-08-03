6e75cbfab33d545e3d9a2bf6653ab88ffba1ad1d
James Butcher <jamesbutcher167@gmail.com>
2021-08-03 01:51:34 +0100

    Add basic velocity

--- include/thunder.cpp
+++ include/thunder.cpp
@@ -1,10 +1,37 @@
 #include "thunder.hpp"
-#include <vector>
 
-PhysicsBodyRec::PhysicsBodyRec(PhysicsEnvironment* env, float x, float y){
-    env->objects.push_back(*this);
+#include <bits/types/time_t.h>
+#include <vector>
+#include <chrono>
+
+#define time_now std::chrono::system_clock::now
+
+/* PhysicsBodyRec */
+
+PhysicsBodyRec::PhysicsBodyRec(PhysicsEnvironment* env, Vec2 position, Vec2 velocity){
+  this->position = position;
+  this->velocity = velocity;
+  env->objects.push_back(this);
 }
 
+void PhysicsBodyRec::update(){
+  if (lastUpdate.isSet){
+    std::chrono::duration<double> dur = time_now() - this->lastUpdate.time;
+    this->position.x += this->velocity.x * dur.count();
+    this->position.y += this->velocity.y * dur.count();
+  }
+
+  this->lastUpdate.time = time_now();
+  this->lastUpdate.isSet = true;
+}
+
+/* PhysicsEnvironment */
+
 PhysicsEnvironment::PhysicsEnvironment(float gravity){
-    this->gravity = gravity;
+  this->gravity = gravity;
+}
+
+void PhysicsEnvironment::update(){
+  for (auto i : this->objects)
+    i->update();
 }
--- include/thunder.hpp
+++ include/thunder.hpp
@@ -1,20 +1,39 @@
 #pragma once
-#include <vector>
 
+#include <chrono>
+#include <cstddef>
+#include <vector>
 
 class PhysicsEnvironment;
 class PhysicsBodyRec;
 
+struct Vec2{
+  float x;
+  float y;
+};
+
+struct time_point{
+  std::chrono::time_point<std::chrono::system_clock> time;
+  bool isSet = false;
+};
+
 class PhysicsBodyRec{
 public:
-    PhysicsBodyRec(PhysicsEnvironment*, float, float);
+  time_point lastUpdate;
+
+  Vec2 position;
+  Vec2 velocity;
+
+  PhysicsBodyRec(PhysicsEnvironment*, Vec2, Vec2);
+  void update();
 };
 
 class PhysicsEnvironment{
 public:
-    float gravity;
+  float gravity;
 
-    std::vector<PhysicsBodyRec> objects;
+  std::vector<PhysicsBodyRec*> objects;
 
-    PhysicsEnvironment(float);
+  PhysicsEnvironment(float);
+  void update();
 };
