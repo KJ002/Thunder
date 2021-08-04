97693a2ab62bd0bb045cae2af0cefed301c0a9c3
James Butcher <jamesbutcher167@gmail.com>
2021-08-05 00:04:25 +0100

    Add check for rotation

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 int main(){
   PhysicsEnvironment env(9.8);
-  PhysicsBodyRec rectangle(&env, (Vec2){190, 200}, (Vec2){0, 0}, 10);
+  PhysicsBodyRec rectangle(&env, (Vec2){190, 200}, (Vec2){0, 0}, 10, 0, 360);
 
   const int screenWidth = 800;
   const int screenHeight = 450;
@@ -12,13 +12,13 @@ int main(){
 
   SetTargetFPS(60);
 
-  rectangle.applyForce((Vec2){1000, 0});
+  rectangle.applyForce((Vec2){10000, 0});
 
   while (!WindowShouldClose()){
     env.update();
     BeginDrawing();
     ClearBackground(RAYWHITE);
-    DrawRectangle(rectangle.position.x, rectangle.position.y, 20, 20, BLUE);
+    DrawRectanglePro((Rectangle){(float)rectangle.position.x, (float)rectangle.position.y, 20, 20}, (Vector2){10, 10}, rectangle.rotation, BLUE);
     EndDrawing();
   }
 
