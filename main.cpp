055bfcc02309f33a80a467f7d1985f8913de1d61
James Butcher <jamesbutcher167@gmail.com>
2021-08-05 01:54:38 +0100

    Anotate steps

--- main.cpp
+++ main.cpp
@@ -2,27 +2,49 @@
 #include "include/thunder.hpp"
 
 int main(){
+
+  /* Setup Bodies */
+
   PhysicsEnvironment env(9.8);
-  PhysicsBodyRec rectangle(&env, (Vec2){190, 200}, (Vec2){0, 0}, 10, 0, 360);
+
+  PhysicsBodyRec testPhysicsBody(&env, (Vec2){190, 200}, (Vec2){0, 0}, 10, 0, 360);
+  Rectangle testRenderBody = {(float)testPhysicsBody.position.x, (float)testPhysicsBody.position.y, 20, 20};
+
+  /* Window Setup */
 
   const int screenWidth = 800;
   const int screenHeight = 450;
 
-  InitWindow(screenWidth, screenHeight, "test");
+  InitWindow(screenWidth, screenHeight, "Thunder Test Environment");
 
   SetTargetFPS(60);
 
-  rectangle.applyForce((Vec2){10000, 0});
+  /* Test Linear Velocity */
+
+  testPhysicsBody.applyForce((Vec2){0, 9.8});
 
   while (!WindowShouldClose()){
+
+    /* Update Physics Environment  */
+
     env.update();
+
+    /* Update Render Body With Physics Body */
+
+    testRenderBody.x = (float)testPhysicsBody.position.x;
+    testRenderBody.y = (float)testPhysicsBody.position.y;
+
+    /* Draw Render Object */
+
     BeginDrawing();
-    ClearBackground(RAYWHITE);
-    DrawRectanglePro((Rectangle){(float)rectangle.position.x, (float)rectangle.position.y, 20, 20}, (Vector2){10, 10}, rectangle.rotation, BLUE);
+        ClearBackground(RAYWHITE);
+        DrawRectanglePro((Rectangle){(float)testPhysicsBody.position.x, (float)testPhysicsBody.position.y, 20, 20},
+                         (Vector2){10, 10}, testPhysicsBody.rotation, BLUE);
     EndDrawing();
   }
 
   CloseWindow();
 
   return 0;
+
 }
